9791413aa71e1fafdbffadf6f0713a8a722f74fa
xiety <qyyzxty@icloud.com>
2025-05-05 00:25:27 +0800

    [3.15]--打开分页机制(3)

--- source/os.c
+++ source/os.c
@@ -1,22 +1,21 @@
-/**
- * 功能：32位代码，完成多任务的运行
- *
- *创建时间：2022年8月31日
- *作者：李述铜
- *联系邮箱: 527676163@qq.com
- *相关信息：此工程为《从0写x86 Linux操作系统》的前置课程，用于帮助预先建立对32位x86体系结构的理解。整体代码量不到200行（不算注释）
- *课程请见：https://study.163.com/course/introduction.htm?courseId=1212765805&_trace_c_p_k2_=0bdf1e7edda543a8b9a0ad73b5100990
- */
 #include "os.h"
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
-#degine PDE_P   (1 << 0)
-#degine PDE_W   (1 << 1)
-#degine PDE_U   (1 << 2)
-#degine PDE_PS  (1 << 7)
+#define PDE_P   (1 << 0)
+#define PDE_W   (1 << 1)
+#define PDE_U   (1 << 2)
+#define PDE_PS  (1 << 7)
+
+#define MAP_ADDR 0x80000000
+
+uint8_t map_phy_buffer[4096] __attribute__((aligned(4096))) = { 0x36 };
+
+// 页表项
+static uint32_t page_table[1024] __attribute__((aligned(4096))) = { PDE_U };
+
 // 页目录表
 uint32_t page_dir[1024] __attribute__((aligned(4096))) = {
     [0] = (0) | PDE_P | PDE_W | PDE_U | PDE_PS
@@ -43,3 +42,8 @@ struct {
         .base_limit = 0x00CF    // 0x00表示段的属性，0xCF表示段的属性，0x00表示段的属性
     }
 };
+
+void os_init(void) {
+    page_dir[MAP_ADDR >> 22] = (uint32_t)page_table | PDE_P | PDE_W | PDE_U;
+    page_table[MAP_ADDR >> 12 & 0x3FF] = (uint32_t)map_phy_buffer | PDE_P | PDE_W | PDE_U;
+}
